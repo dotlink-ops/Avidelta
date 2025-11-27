d22108f5e01fea487d9e7aed22be4809b7e89334
Kamar Foster <fosterkamar33@gmail.com>
2025-11-26 20:36:36 -0800

    Add env example and configure environment usage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const { NEXT_PUBLIC_ENV, NEXT_PUBLIC_SITE_URL, NEXT_PUBLIC_ANALYTICS_ID } =
+  process.env;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_ENV,
+    NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_ANALYTICS_ID,
+  },
 };
 
 export default nextConfig;
